17b08338db23174cc95675f2841ba19a9e20f93d
SergProgMan <semibratyev@GMAIL.COM>
2017-03-20 21:32:51 +0200

    initial commit

---
+++ Assets/Scripts/MazeCell.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class MazeCell : MonoBehaviour {
+	public IntVector2 coordinates;
+
+	private MazeCellEdge[] edges = new MazeCellEdge[MazeDirections.Count];
+
+	public MazeCellEdge GetEdge (MazeDirection direction){
+		return edges [(int)direction];
+	}
+
+	public void SetEdge (MazeDirection direction, MazeCellEdge edge){
+		edges [(int)direction] = edge;
+	}
+}
